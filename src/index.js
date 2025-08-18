de637464ad136c8a898684f126ea0e7c25785057
bfrye1973 <brianfrye23@gmail.com>
2025-08-18 06:43:48 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import OverlayLab from "./pages/OverlayLab.jsx";
-
-function Root() {
-  const hash = typeof window !== "undefined" ? window.location.hash : "";
-  if (hash.startsWith("#/overlay-lab")) {
-    return <OverlayLab />;
-  }
-  return <App />;
-}
+import LiveFeedsPage from "./pages/livefeeds.jsx";
 
 const el = document.getElementById("root");
-createRoot(el).render(<Root />);
+createRoot(el).render(<LiveFeedsPage />);
