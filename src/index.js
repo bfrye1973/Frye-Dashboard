fa6a3e7a3b47d6b9d78945708836166d9a4cef71
bfrye1973 <brianfrye23@gmail.com>
2025-08-18 06:05:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // src/index.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import OverlayLab from "./pages/OverlayLab.jsx";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+function Root() {
+  const hash = typeof window !== "undefined" ? window.location.hash : "";
+  if (hash.startsWith("#/overlay-lab")) {
+    return <OverlayLab />;
+  }
+  return <App />;
+}
+
+const el = document.getElementById("root");
+createRoot(el).render(<Root />);
