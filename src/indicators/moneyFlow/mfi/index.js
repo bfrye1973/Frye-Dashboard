0ad16893b1eb3ec7449d71609ba2054f2bb2d054
bfrye1973 <brianfrye23@gmail.com>
2025-08-19 07:50:29 -0700

    Update index.js

--- src/indicators/moneyFlow/mfi/index.js
+++ src/indicators/moneyFlow/mfi/index.js
@@ -1,1 +1,16 @@
+import { INDICATOR_KIND } from "../../shared/indicatorTypes";
+import { mfiCompute } from "./compute";
+import { mfiAttach } from "./pane";
+import { mfiDefaults } from "./schema";
+
+const MFI = {
+  id: "mfi",
+  label: "Money Flow Index",
+  kind: INDICATOR_KIND.SEPARATE,
+  compute: mfiCompute,
+  attach: mfiAttach,
+  defaults: mfiDefaults,
+};
+
+export default MFI;
 
