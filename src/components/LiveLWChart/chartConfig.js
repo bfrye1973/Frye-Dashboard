52f0e012fff6519d35f965fe0bb4e73150575359
bfrye1973 <brianfrye23@gmail.com>
2025-08-19 08:05:16 -0700

    Create chartConfig.js

---
+++ src/components/LiveLWChart/chartConfig.js
@@ -0,0 +1,8 @@
+// src/components/LiveLWChart/chartConfig.js
+export const baseChartOptions = {
+  layout: { textColor: "#9BA3AF", background: { type: "Solid", color: "#0B1220" } },
+  crosshair: { mode: 1 },
+  grid: { vertLines: { visible: false }, horzLines: { visible: false } },
+  rightPriceScale: { borderVisible: false },
+  timeScale: { borderVisible: false, secondsVisible: false },
+};
