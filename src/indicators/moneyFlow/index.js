398de8a68f8da054bcb9f006057dd23437b0c723
bfrye1973 <brianfrye23@gmail.com>
2025-08-19 07:49:42 -0700

    Update index.js

--- src/indicators/moneyFlow/index.js
+++ src/indicators/moneyFlow/index.js
@@ -1,1 +1,11 @@
+// Collect and export Money Flow indicators (MFI, CMF)
+import MFI from "./mfi";
+import CMF from "./cmf";
+
+export const MONEY_FLOW_INDICATORS = [MFI, CMF];
+
+export const MONEY_FLOW_MAP = MONEY_FLOW_INDICATORS.reduce((acc, ind) => {
+  acc[ind.id] = ind;
+  return acc;
+}, {});
 
