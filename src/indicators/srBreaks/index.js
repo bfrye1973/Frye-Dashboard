b6c85c2a33dab4df6b77507d18b49d223efb3cdc
bfrye1973 <brianfrye23@gmail.com>
2025-08-19 20:29:54 -0700

    Update index.js

--- src/indicators/srBreaks/index.js
+++ src/indicators/srBreaks/index.js
@@ -1,9 +1,19 @@
-import srBreaksIndicators from "./srBreaks";
+// src/indicators/srBreaks/index.js
+import { INDICATOR_KIND } from "../shared/indicatorTypes";   // NOTE: ../shared (not ../../shared)
+import { srDefaults } from "./schema";
+import { srCompute } from "./compute";
+import { srAttach } from "./overlay";
 
-export const INDICATORS = [
-  ...asArray(moneyFlowIndicators),
-  ...asArray(emaIndicators),
-  ...asArray(volumeIndicators),
-  ...asArray(srBreaksIndicators),  // add here
-];
+const SRBREAKS = {
+  id: "sr_breaks",
+  label: "Support/Resistance Breaks",
+  kind: INDICATOR_KIND.OVERLAY, // draw on price pane
+  defaults: srDefaults,
+  compute: (candles, inputs) => srCompute(candles, { ...srDefaults, ...(inputs || {}) }),
+  attach: (chartApi, seriesMap, result, inputs) =>
+    srAttach(chartApi, seriesMap, result, { ...srDefaults, ...(inputs || {}) }),
+};
 
+// export as ARRAY for the master registry flattener
+const srBreaksIndicators = [SRBREAKS];
+export default srBreaksIndicators;
