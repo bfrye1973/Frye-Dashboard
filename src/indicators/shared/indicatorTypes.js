57b3b9221144671107cbb1da80385b2a8ddf0ca0
bfrye1973 <brianfrye23@gmail.com>
2025-08-20 15:33:04 -0700

    Update indicatorTypes.js

--- src/indicators/shared/indicatorTypes.js
+++ src/indicators/shared/indicatorTypes.js
@@ -1,4 +1,10 @@
-export const INDICATOR_KIND = {
-  OVERLAY: "overlay",
-  SEPARATE: "separate",
+// Central list of indicator IDs so we don't mistype them
+const indicatorTypes = {
+  EMA10: "ema10",
+  EMA20: "ema20",
+  MFP: "mfp",       // Money Flow Profile
+  SR: "sr",         // Support / Resistance
+  SWING: "swing",   // Swing Points & Liquidity
 };
+
+export default indicatorTypes;
