38e67b062e7fd3973b0dfcb8e178d4a2f0289731
bfrye1973 <brianfrye23@gmail.com>
2025-08-20 19:50:47 -0700

    Delete src/indicators/volume directory

--- src/indicators/volume/compute.js
+++
@@ -1,4 +0,0 @@
-export function volumeCompute(candles) {
-  if (!candles?.length) return [];
-  return candles.map(c => ({ time: c.time, value: c.volume ?? 0, color: (c.close >= c.open) ? undefined : undefined }));
-}
