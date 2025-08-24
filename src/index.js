63bb895bd9eb84b03819a20ec57a7e91ed072933
bfrye1973 <brianfrye23@gmail.com>
2025-08-24 07:33:40 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
-// src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// 🔵 Build tag from React bundle (hard to miss)
-document.title = "Frye Dashboard • BUILD RB2";
+const el = document.getElementById("root");
+createRoot(el).render(<App />);
 
-function BuildBanner() {
-  return (
-    <div style={{
-      background:"#111827",
-      borderBottom:"1px solid #334155",
-      padding:"6px 10px",
-      font:"12px/1.4 system-ui",
-      color:"#93a3b8"
-    }}>
-      BUILD TAG: <strong>RB2</strong>
-    </div>
-  );
-}
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BuildBanner />
-    <App />
-  </React.StrictMode>
-);
 
 
