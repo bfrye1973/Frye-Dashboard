24f287208bb5e31da0a0d15b600fcb72eb6bc3e7
bfrye1973 <brianfrye23@gmail.com>
2025-09-01 11:00:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, { useEffect, useState } from "react";
-import FerrariCluster from "./components/FerrariCluster";
-import { subscribeGauges, subscribeSignals } from "./services/tos";
+// src/App.jsx
+import React from "react";
+import GaugeCluster from "./components/GaugeCluster";
 
 export default function App() {
-  const [gauges, setGauges] = useState({ rpm:5200, speed:68, water:62, oil:55, fuel:73 });
-  const [lights, setLights] = useState({ breakout:false, buy:false, sell:false, emaCross:false, stop:false, trail:false });
-
-  useEffect(() => {
-    const stopG = subscribeGauges(setGauges);
-    const stopS = subscribeSignals(setLights);
-    return () => { stopG?.(); stopS?.(); };
-  }, []);
-
   return (
-    <div style={{ padding: 14 }}>
-      <FerrariCluster
-        rpm={gauges.rpm}
-        speed={gauges.speed}
-        water={gauges.water}
-        oil={gauges.oil}
-        fuel={gauges.fuel}
-        lights={lights}
-        height={360} // tweak if you want taller/shorter
-      />
-      {/* your chart/journal/strategies go here */}
+    <div className="min-h-screen bg-black">
+      <div className="max-w-6xl mx-auto p-4">
+        <GaugeCluster />
+      </div>
     </div>
   );
 }
