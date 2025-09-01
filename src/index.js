d5217a913bcb219533487bcdaf10e2cd33ad3034
bfrye1973 <brianfrye23@gmail.com>
2025-09-01 13:55:04 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// src/index.js  (CRA entry)
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById("root");
+createRoot(el).render(<App />);
