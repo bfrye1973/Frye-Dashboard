030a1f2b44d6cfc4729c7985a8f90cb8b3f1ee6e
bfrye1973 <brianfrye23@gmail.com>
2025-09-01 12:47:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // src/App.jsx
 import React from "react";
 import GaugeCluster from "./components/GaugeCluster";
+import "./index.css";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-black">
-      <div className="max-w-6xl mx-auto p-4">
+    <div className="page">
+      <div className="container">
         <GaugeCluster />
       </div>
     </div>
