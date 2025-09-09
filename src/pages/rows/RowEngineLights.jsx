61979f6e39bb95ecd6649cdd0e2dbd474f92d730
bfrye1973 <brianfrye23@gmail.com>
2025-09-09 10:32:02 -0700

    Create  RowEngineLights.jsx

---
+++ src/pages/rows/RowEngineLights.jsx
@@ -0,0 +1,13 @@
+// src/pages/rows/RowEngineLights.jsx  (example)
+import React from "react";
+
+export default function RowEngineLights() {
+  return (
+    <section id="row-3" className="panel" style={{ marginTop: 12 }}>
+      <div className="panel-head">
+        <div className="panel-title">Engine Lights</div>
+      </div>
+      <div className="muted small">Active signals will appear here.</div>
+    </section>
+  );
+}
