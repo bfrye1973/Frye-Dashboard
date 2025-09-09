a66c59908ee231d0243b4929b6baa3bfaea8dc50
bfrye1973 <brianfrye23@gmail.com>
2025-09-09 10:01:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+// src/App.js
 import React from "react";
 import NewDashboard from "./pages/NewDashboard";
+import ErrorBoundary from "./ErrorBoundary";
 import "./index.css";
+
 export default function App() {
-  return <div style={{ minHeight: "100vh" }}><NewDashboard /></div>;
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <ErrorBoundary>
+        <NewDashboard />
+      </ErrorBoundary>
+    </div>
+  );
 }
