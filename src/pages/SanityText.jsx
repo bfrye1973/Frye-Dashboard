17d294fa1ab8c52148a7090018fed759c724d53b
bfrye1973 <brianfrye23@gmail.com>
2025-09-09 09:29:24 -0700

    Delete src/pages/SanityText.jsx

--- src/pages/SanityText.jsx
+++
@@ -1,8 +0,0 @@
-export default function SanityText() {
-  return (
-    <div style={{ color: "#0f0", padding: 20 }}>
-      Sanity OK ✅
-      <div>Routing works and React is rendering.</div>
-    </div>
-  );
-}
