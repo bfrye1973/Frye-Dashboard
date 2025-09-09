c0b3987f377101bfeb85825bbca1c440498940ac
bfrye1973 <brianfrye23@gmail.com>
2025-09-09 09:54:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-// src/App.js
 import React from "react";
 import NewDashboard from "./pages/NewDashboard";
 import "./index.css";
-
 export default function App() {
-  return (
-    <div style={{ minHeight: "100vh" }}>
-      <NewDashboard />
-    </div>
-  );
+  return <div style={{ minHeight: "100vh" }}><NewDashboard /></div>;
 }
