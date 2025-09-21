9a9e8f35c4db07a99908cfdf9bd608827ed4793c
bfrye1973 <brianfrye23@gmail.com>
2025-09-21 15:08:16 -0700

    Delete src/api/annotations.js

--- src/api/annotations.js
+++
@@ -1,9 +0,0 @@
-import { USE_MOCK, apiUrl, jsonGet } from "./base";
-import { mockAnnotations } from "../pages/rows/RowStrategies/mocks/mockAnnotations";
-
-
-export async function fetchAnnotations({ symbol, strategy, tf }) {
-if (USE_MOCK) return mockAnnotations({ symbol, strategy, tf });
-const qs = new URLSearchParams({ symbol, strategy, tf }).toString();
-return jsonGet(apiUrl(`/annotations?${qs}`));
-}
