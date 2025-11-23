5d1f8fda9d534f8b00d5eb436c7b5be8e136920a
bfrye1973 <brianfrye23@gmail.com>
2025-11-23 13:29:50 -0700

    Update index.jsx

--- src/pages/rows/RowChart/index.jsx
+++ src/pages/rows/RowChart/index.jsx
@@ -891,7 +891,7 @@ export default function RowChart({
           display: "flex",
           flexDirection: "row",
           width: "100%",
-          height: "100%",
+          // height: "100%",  ← removed this line
         }}
       >
         <div
