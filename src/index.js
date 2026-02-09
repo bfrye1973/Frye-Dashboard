775767cc6daf20038662009299618340533c3ae0
bfrye1973 <brianfrye23@gmail.com>
2026-02-09 12:44:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,11 @@ import App from "./App";
 import "./index.css";
 import "./theme.css";
 
-import LockedDashboardCanvas from "./LockedDashboardCanvas";
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <LockedDashboardCanvas>
-      <App />
-    </LockedDashboardCanvas>
+    <App />
   </React.StrictMode>
 );
